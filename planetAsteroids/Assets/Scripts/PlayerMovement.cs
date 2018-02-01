d2c8e428c7c90d38a584ef53c53f5c1993039db6
Lee Kempton <lee.kempton@students.plymouth.ac.uk>
2018-02-01 17:51:50 +0000

    Added player movement.

---
+++ planetAsteroids/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float maxSpeed = 5.0f;
+    [SerializeField]
+    private float maxRotationSpeed = 5.0f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 newForce = new Vector3(0.0f, moveVertical);
+        Vector3 newRotation = new Vector3(0.0f, 0.0f, moveHorizontal);
+
+        rb.AddRelativeForce(newForce, ForceMode.Force);
+
+        rb.AddTorque(newRotation, ForceMode.Force);
+
+        // Cap the max forwards thrust.
+        if (rb.velocity.y >= maxSpeed)
+            rb.velocity = new Vector3(rb.velocity.x, maxSpeed);
+
+        // Cap the max rotational thrust.
+        if (rb.velocity.x >= maxRotationSpeed)
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, maxRotationSpeed);
+    }
+}
