cc4013f377cacbc9ec5fe11c850c068572392f56
Lee Kempton <lee.kempton@students.plymouth.ac.uk>
2018-02-01 18:36:02 +0000

    Added artificial gravity to the planet, made change it due to issues with rotation.

---
+++ planetAsteroids/Assets/Scripts/ArtificalGravity.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtificalGravity : MonoBehaviour {
+
+    [SerializeField]
+    private Transform planet;
+
+    private Rigidbody rb;
+
+    [SerializeField]
+    private float gravity = 9.81f;
+
+    [SerializeField]
+    private float stopDistance = 10f;
+
+    [SerializeField]
+    private float maxRotationSpeed = 5.0f;
+
+    private void Awake()
+    {
+        planet = GameObject.FindWithTag("Planet").GetComponent<Transform>();
+
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float dist = Vector3.Distance(rb.position, planet.position);
+
+        if (dist > stopDistance)
+        rb.position = Vector3.MoveTowards(rb.position, planet.position, gravity * Time.deltaTime);
+
+    }
+
+
+}
